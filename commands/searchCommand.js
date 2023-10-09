b1c108127f15f7de8d4a9889cdf0ad4dc0e06a79
EvanGatte <106684791+evangatte@users.noreply.github.com>
2023-10-08 19:00:14 -0500

    add search command to index.js

--- commands/searchCommand.js
+++ commands/searchCommand.js
@@ -15,6 +15,6 @@ export const searchCommand = {
 			})
 	},
 	handler: (argv) => {
-
+		console.log("search")
 	}
 }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { hideBin } from "yargs/helpers";
 import yargs from "yargs/yargs";
 import { openCommand } from "./commands/openCommand.js";
+import { searchCommand } from "./commands/searchCommand.js";
 
 const argv = yargs(hideBin(process.argv))
 	.command(openCommand)
+	.command(searchCommand)
 	.help()
 	.argv;

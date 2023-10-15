35d11f5cec5761b48313edf15a84b39eeafcd4c9
EvanGatte <106684791+evangatte@users.noreply.github.com>
2023-10-14 23:13:47 -0500

    remove unused code

--- src/uri/Uri.ts
+++ src/uri/Uri.ts
@@ -2,12 +2,6 @@ import { Arguments } from "yargs";
 import { buildOpenUri } from "./openUri.js";
 import { buildSearchUri } from "./searchUri.js";
 
-enum Actions {
-	Open = "open",
-	Search = "search",
-	New = "new"
-}
-
 export class Uri {
 	static buildOpenUri(argv: Arguments) {
 		return buildOpenUri(argv);

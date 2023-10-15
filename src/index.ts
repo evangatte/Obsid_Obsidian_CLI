61fddea14f9ee0a0f41f9f603cbe870306bc950f
EvanGatte <106684791+evangatte@users.noreply.github.com>
2023-10-14 23:38:02 -0500

    create help command

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { hideBin } from "yargs/helpers";
 import yargs from "yargs/yargs";
 import { openCommand } from "./commands/openCommand.js";
 import { searchCommand } from "./commands/searchCommand.js";
+import { defaultCommand } from "./commands/defaultCommand.js";
 
 const argv = yargs(hideBin(process.argv))
 	.command(openCommand)
 	.command(searchCommand)
+	.command(defaultCommand)
 	.help(false)
 	.argv;
